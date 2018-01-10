ffb8f77e80d77a18c1b0965a298aff5cf3460c3a
C.T. Lin <chentsulin@gmail.com>
2018-01-10 18:33:17 +0800

    fix quote

--- rules/__tests__/prefer-spacing.spec.js
+++ rules/__tests__/prefer-spacing.spec.js
@@ -9,6 +9,7 @@ ruleTester.run('prefer-spacing', rules['prefer-spacing'], {
   valid: [
     'var text = "Mr. 龍島主道：「Let\'s Party! 各位高明博雅君子！」"',
     'var text = "Sephiroth 見他這等神情, 也是悚然一驚: 不知我這 Ultimate Destructive Magic 是否對付得了?"',
+    "var text = 'Sephiroth 見他這等神情, 也是悚然一驚: 不知我這 Ultimate Destructive Magic 是否對付得了?'",
     'var text = "為什麼叫開放政府？\\n基本精神就是政治重新為人民所共有。全民參與，就是落實真正的民主。"',
     'var text = "我其實對反年改團體是，怎麼講，抱歉又同情\\n我雖然支持年金改革，但我覺得法律溯及既往是違反法律的原則"',
   ],
--- rules/prefer-spacing.js
+++ rules/prefer-spacing.js
@@ -7,12 +7,15 @@ module.exports = context => {
     Literal(node) {
       if (typeof node.value === 'string') {
         const raw = node.raw;
-        const spaced = JSON.stringify(
-          JSON.parse(node.raw)
-            .split('\n')
+        let quote = raw[0];
+        const spaced =
+          quote +
+          node.raw
+            .slice(1, node.raw.length - 1)
+            .split('\\n')
             .map(l => pangu.spacing(l).trim())
-            .join('\n')
-        );
+            .join('\\n') +
+          quote;
 
         if (spaced !== raw) {
           context.report({
